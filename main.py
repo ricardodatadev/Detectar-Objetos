6f198fc86882c64d0c9475ac0f31c3c471e1f020
Ricardo Pereira <ricardodatadev@gmail.com>
2025-02-05 22:37:52 -0300

    Aplicando Where

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from ultralytics import YOLO
 model = YOLO('yolo11n.pt')
 
 video = cv2.VideoCapture('video2.mp4')
+
+while video.isOpened():
+    check, img = video.read()
+    img = cv2.resize(img, (1280, 720))
+    result = model(img)
