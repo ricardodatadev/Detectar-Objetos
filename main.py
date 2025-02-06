01fb6ece40b9dc28ee5b996a6b8bfaaa6094d7fa
Ricardo Pereira <ricardodatadev@gmail.com>
2025-02-05 22:39:07 -0300

    Finalizando o código

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ while video.isOpened():
     check, img = video.read()
     img = cv2.resize(img, (1280, 720))
     result = model(img)
+    
+    for r in result:
+        boxes = r.boxes
+        clas = boxes.cls
+        xy = boxes.xyxy.int()
+
+
+        for box, cls in zip(xy, clas):
+            if int(cls) ==7:
+                x_min, y_min, x_max, y_max, = box.tolist()
+                cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (255, 0, 0), 3)
+
+        cv2.imshow("Detect", img)
+        cv2.waitKey(1)
